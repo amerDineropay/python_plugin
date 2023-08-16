0dff0c9b820f15536a0c183eea05a23b7c86cfa6
Yahya-Moallim <139042492+Yahya-Moallim@users.noreply.github.com>
2023-08-16 10:32:08 +0300

    added setup.py (#3)
    initializing the setup.py file 
    Co-authored-by: yahya moallim <yhy_565@hotmail.com>

---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README", 'r') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="dineroPay",                     # This is the name of the package
+    version="0.0.1",                        # The initial release version
+    author="DineroPay",                     # Full name of the author
+    description="Providing Dinero Pay services",
+    # Long description read from the the readme file
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    # List of all python modules to be installed
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],                                      # Information to filter the project on PyPi website
+    url='***',                     # Link to your github repository or website:
+    python_requires='>=3.6',                # Minimum version requirement of the package
+    py_modules=["dineroPay"],             # Name of the python package
+    # Directory of the source code of the package
+    package_dir={'': '.'},
+    # Install other dependencies if any
+    install_requires=['json', 'requests', 'hashlib']
+)
